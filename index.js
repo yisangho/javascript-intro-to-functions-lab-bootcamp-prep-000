6ba2818d065a2bc8572355a47c43942701a9f1d4
yisangho <yisangho717@gmail.com>
2018-08-21 03:20:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logShout(string) {
 }
 
 function logWhisper(string) {
-  return console.log()string.toLowerCase())
+  return console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
