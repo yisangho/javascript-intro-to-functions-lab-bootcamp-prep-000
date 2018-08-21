d761104941a79d91ecb5801c2f1d04e82388e2da
yisangho <yisangho717@gmail.com>
2018-08-21 03:26:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+  if(string == 'hello') {
+    return "I can't hear you!"
+  }
   if(string == 'HELLO') {
     return "YES INDEED!"
+  }
+  if(string == 'I love you, Grandma.') {
+    return "I love you, too."
   }
 }
